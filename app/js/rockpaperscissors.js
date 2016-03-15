0184bd8f76c0774cddd9acc4afb1edf1bb4c0edb
Natalie Djerf <nataliedjerf@Natalies-MacBook-Air.local>
2016-03-15 09:20:37 -0400

    fixed homework 4

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -32,7 +32,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move || randomPlay()
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -43,7 +43,32 @@ function getWinner(playerMove,computerMove) {
     /* YOUR CODE HERE */
     var playerMove = getPlayerMove();
 	var computerMove = getComputerMove();
-	if (playerMove === 'rock' && computerMove === 'scissors'){
+	console.log('Player move is ' + playerMove + ' & computer move is ' + computerMove)
+	
+if ((playerMove === 'rock' && computerMove === 'scissors') || (playerMove === 'scissors' && computerMove === 'paper') || (playerMove === 'paper' && computerMove === 'rock')) {
+		winner = 'player'
+}
+else if ((playerMove === 'scissors' && computerMove === 'rock') || (playerMove === 'paper' && computerMove ==='scissors') || (playerMove === 'rock' && computerMove === 'paper')) {
+		winner = 'computer'
+}
+else {
+	winner = 'tie'
+}
+return winner;
+}
+
+
+
+
+
+
+
+
+
+
+	/* if (playerMove === computerMove) {
+		winner = 'tie';
+	} else if (playerMove === 'rock' && computerMove === 'scissors'){
 		winner = 'player';
 	} else if (playerMove === 'rock' && computerMove === 'paper'){
 		winner = 'computer';
@@ -55,12 +80,10 @@ function getWinner(playerMove,computerMove) {
 		winner = 'player';
 	} else if (playerMove === 'scissors' && computerMove === 'paper'){
 		winner = 'player';
-    } else {
-        winner = 'tie'
     }
 
     return winner;
-}
+} */
 
 // Do I need to define the variables? 
 /*  var playerMove;
@@ -69,14 +92,43 @@ OR:
     var playerMove = getPlayerMove();
 	var computerMove = getComputerMove();
 Or:
-	var playerMove = ('rock' || 'paper' || 'scissors')  */
+	var playerMove = ('rock' || 'paper' || 'scissors')  
+
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    // After each 'round', display some text in the console indicating who played
+      // what, who won, and what the current scoreboard looks like.
+  	// For example,
+  	//  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+ 	 //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n')
+while (playerWins < 5 && computerWins < 5){
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    var winner = getWinner(playerMove,computerMove);
+
+    if (winner === 'player'){
+        playerWins =+ 1;
+    console.log('player played ' + playerMove + 'while computer played ' + computerMove)
+    console.log('player win count is ' + playerWins)
+    }
+    else if (winner === 'computer'){
+        computerWins = +1;
+    console.log('player played '; + playerMove + 'while computer played ' + computerMove)
+    console.log('computer win count is ' + computerWins)
+    }
+    else {
+        computerWins =+ 0;
+        playerWins =+ 0;
+    console.log('player played ' + playerMove 'while computer played ' + computerMove + ', therefore a tie')
+    }
+
     return [playerWins, computerWins];
+
 }
+}
+*/
 
